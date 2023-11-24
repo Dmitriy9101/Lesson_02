db44922387abb94ffabb7c62f1d5b83cb180ca71
Dmitriy <dmitriy.chicunov@gmail.com>
2023-11-24 23:07:50 +0300

    Решили задачу с макс цифрой рандомного числа

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi, World");
-asd
+﻿// Напишите программу которая выводит случайное число из отрезка
+// (10,99) и показывает наибольшую цифру числа
+
+int number = new Random().Next(10, 100); //[10,99] или [10,100)
+// Console.WriteLine($"Рандомное число из диапазона [10,99]: {number}");
+
+int firstDigit = number / 10; // 78/10 = 7
+int secondDigit = number % 10; // 78%10 = 8
+
+// Console.WriteLine($"1 цифра: {firstDigit}, 2 цифра: {secondDigit}");
+
+int max = firstDigit; // Пусть максимум - 1 цифра числа
+if (secondDigit > max) // 8 > max(7)
+{
+    max = secondDigit; // max=8
+}
+Console.WriteLine($"Максимальная цифра в числе {number}: {max}");
